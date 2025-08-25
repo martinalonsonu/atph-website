f7eae17f26afe1d19cd8e5a3514ca26a4fe4af2a
martinalonsonu <martin99huacho@gmail.com>
2025-08-24 20:55:45 -0500

    fix: new hot fix

--- src/app/publicaciones/[slug]/page.tsx
+++ src/app/publicaciones/[slug]/page.tsx
@@ -1,7 +1,11 @@
 import PostPage from "@/features/post";
 
-export default async function Post({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
 
   return <PostPage slug={slug} />;
 }
