45f5eda5cac4bf9808cec50d983252d37dff90b6
Martin Alonso Núñez Navarro <114968362+martinalonsonu@users.noreply.github.com>
2025-12-12 12:50:32 -0500

    fix: update base path

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     domains: ["atphdev.wordpress.com"],
     unoptimized: true,
   },
-  basePath: isProd ? "/atph-website" : "",
+  basePath: "",
   assetPrefix: "",
 };
 
